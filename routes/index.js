c2edc3dd66ede79b53c0d637fc442906be6061a9
jane <ihenachojane@gmail.com>
2021-06-17 13:50:25 +0100

    initial app setup

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+//for configuring routes that stand-alone eg dashboard, home, etc
+
+const express = require('express')
+const router = express.Router()
+
+//@desc Login/Landing page
+//@route GET /
+
+router.get('/', (req, res) => {
+    //res.send('Login')
+    res.render('login');
+})
+
+//@desc Dashboard
+//@route GET /dashboard
+
+router.get('/dashboard', (req, res) => {
+    //res.send('Dashboard')
+    res.render('dashboard');
+})
+
+
+module.exports = router
